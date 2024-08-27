afe4d5bd7efb6dc781b7e2df83e82771ce5a4a51
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2024-08-27 14:23:20 -0400

    Working on a portfolio landing page

---
+++ Portfolio Page/script.js
@@ -0,0 +1,7 @@
+//Add or remove a html class to the elements below, the styling comes from CSS
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
